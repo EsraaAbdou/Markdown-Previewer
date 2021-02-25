8d4a40ea69f513b38fa81bab77ae702be4644647
Esraa Abdelhady <esraaabdou94@gmail.com>
2021-02-25 16:06:32 +0000

    styles

--- src/App.js
+++ src/App.js
@@ -2,17 +2,23 @@ import React from 'react';
 import './App.css';
 import marked from 'marked';
 
+function Label(props) {
+  return <div className="label font-weight-bold text-light border-bottom-0 py-1 pl-3">{props.title}</div>
+
+}
 function Editor(props) {
   return(
-    <div>
-      <textarea id="editor" name="editor" rows="15" cols="80" value={props.value} onChange={props.handleInputChange}></textarea>
+    <div className="w-50 my-4 mx-auto">
+      <Label title="Editor" className="w-100" />
+      <textarea id="editor" name="editor" rows="10" className="w-100" value={props.value} onChange={props.handleInputChange}></textarea>
     </div>
   );
 }
 function Previewer(props) {
   return(
-    <div>
-      <div id="preview" dangerouslySetInnerHTML={props.content}></div>
+    <div className="w-75 my-0 mx-auto">
+      <Label title="Previewer" className="w-100" />
+      <div id="preview" dangerouslySetInnerHTML={props.content} className="w-100"></div>
     </div>
   );
 }
